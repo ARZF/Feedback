b9a76ad218ba8bf833397fd19d852c4683be7553
ARZF <your.email@example.com>
2024-09-07 08:19:01 +0430

    feedback

--- reviews/views.py
+++ reviews/views.py
@@ -1,29 +1,52 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from .forms import ReviewForm
 from django.views import View
+from django.views.generic.base import TemplateView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
+
+from .models import Review
+from .forms import ReviewForm
 # Create your views here.
-class ReviewView(View):
-    def get(self, request):
-        form = ReviewForm()
-        return render(request, "reviews/review.html", {
-            "form": form
-        })
+class ReviewView(CreateView):
+    model = Review
+    form_class = ReviewForm
+    template_name = "reviews/review.html"
+    success_url = "/thank-you"
 
-    def post(self, request):
-        form = ReviewForm(request.POST)
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
 
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect("/thank-you")
+    # def post(self, request):
+    #     form = ReviewForm(request.POST)
+    #
+    #     if form.is_valid():
+    #         form.save()
+    #         return HttpResponseRedirect("/thank-you")
+    #
+    #     return render(request, "reviews/review.html", {
+    #         "form": form
+    #     })
 
-        return render(request, "reviews/review.html", {
-            "form": form
-        })
+class ThankYouView(TemplateView):
+    template_name = "reviews/thank_you.html"
 
-class ThankYouView(View):
-    def Get (self, request):
-        return render(request, "reviews/thank_you.html")
+    def get_context_data(self):
+        context = super().get_context_data()
+        context ["message"] = "This works"
+        return context
+
+class ReviewsListView(ListView):
+    template_name = "reviews/review_list.html"
+    model = Review
+    context_object_name = "reviews"
+
+class SinglReviewView(DetailView):
+    template_name = "reviews/single_review.html"
+    model = Review
+
+
 
 
 
